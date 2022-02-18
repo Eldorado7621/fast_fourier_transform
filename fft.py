271ad15f3f12ce7737be380ff6a9bccadf922eab
eldorado7621 <akinolasamuel762@gmail.com>
2022-02-18 10:25:49 +0100

    signals extended to three

--- fft.py
+++ fft.py
@@ -7,6 +7,7 @@ Created on Thu Feb 17 17:46:31 2022
 """
 
 import numpy as np
+import random, sys
 import matplotlib.pyplot as plt
 
 
@@ -21,18 +22,26 @@ t=np.linspace(0,(N-1)*t_step,N) #time steps
 
 f_steps=np.linspace(0,(N-1)*f_signal,N)
 
-y=1*np.sin(2*np.pi*f_signal*t)+2*np.cos(2*np.pi*3*f_signal*t)
+#create the signal- I will  be creating 3 different signals
+#the first is a sine wave
+y1=1*np.sin(2*np.pi*f_signal*t)
 
 
-#perform fft
-x=np.fft.fft(y)
-x_mag=np.abs(x)/N
+#the second signal
+y2=1*np.sin(2*np.pi*f_signal*t)+2*np.sin(2*np.pi*3*f_signal*t)
 
-f_plot=f_steps[0:int(N/2+1)]
-x_mag_plot=2*x_mag[0:int(N/2+1)]
-x_mag_plot[0]=x_mag_plot[0]/2
+#the third signal
+np.random.seed(0)   # start from known state
+y3   = np.array([(np.random.randn()+1j*np.random.randn()) for i in range(N)])
 
-#f_plot=f
+#perform fft on the three signals
+x1=np.fft.fft(y1)
+x1_mag=np.abs(x1)/N
+
+x2=np.fft.fft(y2)
+x2_mag=np.abs(x2)/N
+
+x3 = np.fft.fft(y3)
 
 
 
@@ -53,18 +62,26 @@ def dump_mem(filename, x):
 			f.write(di+dr+"\n")
 
 #save the generated data as text file in floating point format  
-np.savetxt("inp_cpp.txt", y, fmt = "%f %f")
-np.savetxt("out_cpp.txt", x, fmt = "%f %f")
+np.savetxt("inp_cpp.txt", y1, fmt = "%f %f")
+np.savetxt("out_cpp.txt", x1, fmt = "%f %f")
 
 #save the generated data in hex format
-dump_mem("inp_hex.mem", y)
-dump_mem("out_hex.mem", x)
+dump_mem("inp_hex.mem", y1)
+dump_mem("out_hex.mem", x1)
 
 
 #plot
+f_plot=f_steps[0:int(N/2+1)]
+x1_mag_plot=2*x1_mag[0:int(N/2+1)]
+x1_mag_plot[0]=x1_mag_plot[0]/2
+
+x2_mag_plot=2*x2_mag[0:int(N/2+1)]
+x2_mag_plot[0]=x2_mag_plot[0]/2
+
+
 fig,[ax1,ax2]=plt.subplots(2,1)
-ax1.plot(t,y,'.-')
+ax1.plot(t,y1,'.-')
 #ax2.plot(f_steps,x_mag,'.-')
 
-ax2.plot(f_plot,x_mag_plot,'.-')
+ax2.plot(f_plot,x1_mag_plot,'.-')
 plt.show()
